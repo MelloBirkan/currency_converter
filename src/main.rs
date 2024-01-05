1bb916e53da56b53ccd2b95ec63c0066f11b72c6
Mello Birkan <mgbirkan@icloud.com>
2024-01-04 22:53:44 -0300

    Update currency converter precision and user interface
    The currency conversion function now provides results up to three decimal points for increased accuracy. The user interface has been revamped with changes to display prompts and program exit command, enhancing the overall user interaction process.

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,9 @@ impl Currency {
         Currency { taxa_conversao, nome }
     }
 
-    fn converter(&self, valor: f64) -> f64 {
+    fn converter(&self, valor: f64) {
         let result = self.taxa_conversao * valor;
-        println!("{} {} = {} BRL", valor, self.nome, result);
-        result
+        println!("{} {} = {:.3} BRL", valor, self.nome, result);
     }
 }
 
@@ -26,8 +25,8 @@ fn get_input(label: &str) -> String {
 }
 
 fn add_currency(moedas: &mut Vec<Currency>) {
-    let name = get_input("Enter the currency name: ");
-    let mut conversion_rate = get_input("Enter the currency conversion rate: ");
+    let name = get_input("Entre o nome da moeda: ");
+    let mut conversion_rate = get_input("Entre com a taixa de conversão da moeda: ");
     if let Some(rate) = conversion_rate.parse::<f64>().ok() {
         moedas.push(Currency::new(rate, name));
     } else {
@@ -40,19 +39,22 @@ fn main() {
 
     // Menu
     let mut menu_input = String::new();
-    while menu_input != "stop" {
+    while menu_input != "sair" {
         menu_input.clear();
-
+        menu_input = get_input("Bem-vindo ao conversor de moedas!\nDigite:\n'adicionar' para adicionar uma moeda\n\
+        'converter' para converter uma moeda de sua escolha para o real\n'sair' para sair");
         match menu_input.as_str() {
             "add" => add_currency(&mut moedas),
-            "stop" => println!("Saindo..."),
+            "converter" => {
+                let moeda = get_input("Digite a moeda que deseja converter: ");
+                let quantidade = get_input("Digite a quantidade de moeda que deseja converter: ");
+                moedas
+                    .iter()
+                    .find(|&x| x.nome == moeda.to_lowercase()).unwrap()
+                    .converter(quantidade.parse::<f64>().unwrap());
+            },
+            "sair" => println!("Saindo..."),
             _ => println!("Opção inválida"),
         }
-        menu_input = get_input("Bem-vindo ao conversor de moedas!\n Digite 'add' para adicionar uma moeda\n \
-        'stop' para sair\n 'converter' para converter uma moeda de sua escolha para o real:");
     }
-
-    add_currency(&mut moedas);
-    println!("Moedas: {:?}", moedas);
-    moedas.iter().find(|&x| x.nome == "Dollar".to_lowercase()).unwrap().converter(10.0);
 }
